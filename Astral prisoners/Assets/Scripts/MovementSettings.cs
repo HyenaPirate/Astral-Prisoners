90886d160fcf1b766c9ead3d587d3b2c295587d2
kukilet <31478793+kukilet@users.noreply.github.com>
2022-03-21 22:54:20 +0100

    Patch
    patched stuff

--- Astral prisoners/Assets/Scripts/MovementSettings.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MovementSettings : MonoBehaviour
-{
-    public bool demon;
-    public bool kot;
-}
---
+++ Astral prisoners/Assets/Scripts/Players/Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+   public Tilemap tilemap;
+   public GameObject player;
+   Vector3 korekta = new Vector3(0.5f, 0.5f, 0);
+    void Update()
+    {
+        if(FindObjectOfType<GameManager>().gameIsPaused == false && player.GetComponent<Activate>().IsActive())
+        {
+            if (Input.GetKeyDown(KeyCode.W) ) //gdy gracz jest aktywny i wcisnie "w"
+            {
+                if (tilemap.GetTile(player.GetComponent<Where>().Pole(0, 1)).name.Substring(0,7) == "Podloga")
+                {
+                    player.transform.position = player.GetComponent<Where>().Pole(0, 1) + korekta;
+                    FindObjectOfType<AudioManager>().Play("Ruch");
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (tilemap.GetTile(player.GetComponent<Where>().Pole(0, -1)).name.Substring(0, 7) == "Podloga")
+                {
+                    player.transform.position = player.GetComponent<Where>().Pole(0, -1) + korekta;
+                    FindObjectOfType<AudioManager>().Play("Ruch");
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (tilemap.GetTile(player.GetComponent<Where>().Pole(-1, 0)).name.Substring(0, 7) == "Podloga")
+                {
+                    player.transform.position = player.GetComponent<Where>().Pole(-1, 0) + korekta;
+                    FindObjectOfType<AudioManager>().Play("Ruch");
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (tilemap.GetTile(player.GetComponent<Where>().Pole(1, 0)).name.Substring(0, 7) == "Podloga")
+                {
+                    player.transform.position = player.GetComponent<Where>().Pole(1, 0) + korekta;
+                    FindObjectOfType<AudioManager>().Play("Ruch");
+                }
+            }
+        }
+    }
+}
