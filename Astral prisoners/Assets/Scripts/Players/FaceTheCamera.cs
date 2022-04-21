1aac00a60aeeade4f93041db3236d2cc78deb0c5
HyenaPirate <lisu.avangard@gmail.com>
2022-04-21 20:37:19 +0200

    Test Rzutu Izometrycznego i testowe tekstury I guess
    scena test 3D, tam to jest

---
+++ Astral prisoners/Assets/Scripts/Players/FaceTheCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTheCamera : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().flipX = true;
+        transform.position += new Vector3(0, .5f, 0);
+    }
+
+    void Update() 
+    {
+        // Axis X and Y
+        //transform.LookAt(2 * transform.position - Camera.main.transform.position);
+
+        // Axis X only
+        Vector3 targetPostition = new Vector3( this.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z ) ;
+        this.transform.LookAt( targetPostition ) ;
+        
+    }
+
+
+}
