d0b79188d27a752543e5a43fe4831ee8fd576661
kukilet <31478793+kukilet@users.noreply.github.com>
2022-04-22 11:58:23 +0200

    Lampy zabijają
    Lampy są groźne

---
+++ Astral prisoners/Assets/Lamp_Dead.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp_Dead : MonoBehaviour
+{
+    public bool dead_by_normal;
+    public bool dead_by_red;
+    private GameManager gameManager;
+    private void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("LampLight"))
+        {
+            if (collision.gameObject.GetComponentInParent<Lampa>())
+            {
+               if(collision.gameObject.GetComponentInParent<Lampa>().rodzaj == Lampa.Rodzaj.normal && dead_by_normal)
+                {
+                    gameManager.GameOver();
+                }
+                if (collision.gameObject.GetComponentInParent<Lampa>().rodzaj == Lampa.Rodzaj.red && dead_by_red)
+                {
+                    gameManager.GameOver();
+                }
+            }
+        }
+    }
+
+}
