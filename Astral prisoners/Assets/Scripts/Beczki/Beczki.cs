f004d5c9c9a92ae93828de4dde340c02d621ebce
Kussitofu <101656945+Kussitofu@users.noreply.github.com>
2022-05-19 19:10:15 +0200

    Beczki UPDATE + L1, L2, L3 UPDATE
    - Zmieniłem klawisze, którymi przesuwa się beczki na te same którymi się chodzi tj. W, A, S, D. Dzięki temu beczki przesuwają się kiedy będąc obok nich poruszymy się na pole na którym stoi beczka.
    - Jednocześnie od teraz niemożliwym jest stanięcie na beczce(jak tylko to zrobimy to gracz zostanie przesunięty z powrotem na poprzednie pole).
    - Naprawiłem błąd przy ładowaniu poziomu który powodował że jeśli na polu (0,0) była dziura to tworzona w tym miejscu była podłoga a jedna z beczek znikała.
    - Od teraz poziomy L1, L2 i L3 używają prefabów.

--- Astral prisoners/Assets/Scripts/Beczki/Beczki.cs
+++ Astral prisoners/Assets/Scripts/Beczki/Beczki.cs
@@ -9,9 +9,11 @@ public class Beczki : MonoBehaviour
     public Tilemap tilemap;
     public Tile pole;
     Vector3 srodek = new Vector3(0.5f, 0.5f, 0);
+    public GameObject manager;
+    int cofnij,cofnij2;
     void Start()
     {
-
+        manager = GameObject.FindGameObjectWithTag("GameManager");
     }
     void Awake()
     {
@@ -24,32 +26,50 @@ public class Beczki : MonoBehaviour
             if (gracze[i] != null)
                 if (gracze[i].GetComponent<Activate>().IsActive())
                 {
-                    if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[0].bounds))  //kiedy aktywny gracz stoi nad beczka
-                        if(tilemap.GetTile(GetComponent<Where>().Pole(0, 0)).name.Substring(0, 3) != "Dzr") //beczka niemoze byc na polu dziury aby zostac przesunieta
-                            if (Input.GetKeyDown(KeyCode.DownArrow) && (tilemap.GetTile(GetComponent<Where>().Pole(0, -1)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(0, -1)).name.Substring(0, 3) == "Dzr")) //po wcisnieciu lewego controla sprawdza czy beczka bedzie przesunieta na pole podloza lub do dziury
+                    if (tilemap.GetTile(GetComponent<Where>().Pole(0, 0)).name.Substring(0, 3) != "Dzr") //beczka niemoze byc na polu dziury aby zostac przesunieta
+                    {
+                        if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[0].bounds))  //kiedy aktywny gracz stoi nad beczka
+                        {
+                            cofnij = 0;
+                            cofnij2 = 1;
+                            if (Input.GetKeyDown(KeyCode.S) && (tilemap.GetTile(GetComponent<Where>().Pole(0, -1)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(0, -1)).name.Substring(0, 3) == "Dzr")) //po wcisnieciu lewego controla sprawdza czy beczka bedzie przesunieta na pole podloza lub do dziury
                                 transform.position = GetComponent<Where>().Pole(0, -1) + srodek; //przesuniecie beczki w do³
-
-                    if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[1].bounds))  //kiedy aktywny gracz stoi pod beczka
-                        if (tilemap.GetTile(GetComponent<Where>().Pole(0, 0)).name.Substring(0, 3) != "Dzr")
-                            if (Input.GetKeyDown(KeyCode.UpArrow) && (tilemap.GetTile(GetComponent<Where>().Pole(0, 1)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(0, 1)).name.Substring(0, 3) == "Dzr"))
+                        }
+                        if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[1].bounds))  //kiedy aktywny gracz stoi pod beczka
+                        {
+                            cofnij = 0;
+                            cofnij2 = -1;
+                            if (Input.GetKeyDown(KeyCode.W) && (tilemap.GetTile(GetComponent<Where>().Pole(0, 1)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(0, 1)).name.Substring(0, 3) == "Dzr"))
                                 transform.position = GetComponent<Where>().Pole(0, 1) + srodek; //przesuniecie beczki do gory
-
-                    if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[2].bounds))  //kiedy aktywny gracz stoi na prawo od beczki
-                        if (tilemap.GetTile(GetComponent<Where>().Pole(0, 0)).name.Substring(0, 3) != "Dzr")
-                            if (Input.GetKeyDown(KeyCode.LeftArrow) && (tilemap.GetTile(GetComponent<Where>().Pole(-1, 0)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(-1, 0)).name.Substring(0, 3) == "Dzr"))
+                        }
+                        if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[2].bounds))  //kiedy aktywny gracz stoi na prawo od beczki
+                        {
+                            cofnij = 1;
+                            cofnij2 = 0;
+                            if (Input.GetKeyDown(KeyCode.A) && (tilemap.GetTile(GetComponent<Where>().Pole(-1, 0)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(-1, 0)).name.Substring(0, 3) == "Dzr"))
                                 transform.position = GetComponent<Where>().Pole(-1, 0) + srodek; //przesuniecie beczki w lewo
-
-                    if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[3].bounds))  //kiedy aktywny gracz stoi na lewo od beczki
-                        if (tilemap.GetTile(GetComponent<Where>().Pole(0, 0)).name.Substring(0, 3) != "Dzr")
-                            if (Input.GetKeyDown(KeyCode.RightArrow) && (tilemap.GetTile(GetComponent<Where>().Pole(1, 0)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(1, 0)).name.Substring(0, 3) == "Dzr"))
+                        }
+                        if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[3].bounds))  //kiedy aktywny gracz stoi na lewo od beczki
+                        {
+                            cofnij = -1;
+                            cofnij2 = 0;
+                            if (Input.GetKeyDown(KeyCode.D) && (tilemap.GetTile(GetComponent<Where>().Pole(1, 0)).name.Substring(0, 3) == "Pdl" || tilemap.GetTile(GetComponent<Where>().Pole(1, 0)).name.Substring(0, 3) == "Dzr"))
                                 transform.position = GetComponent<Where>().Pole(1, 0) + srodek; //przesuniecie beczki w prawo
+                        }
+                    }
+                    if (gracze[i].GetComponent<Collider2D>().bounds.Intersects(GetComponents<Collider2D>()[4].bounds))  //kiedy aktywny gracz wejdzie na pole z beczka
+                        gracze[i].transform.position = GetComponent<Where>().Pole(cofnij, cofnij2) + srodek;
 
                 }     
         }
-        if (tilemap.GetTile(GetComponent<Where>().pos).name.Substring(0, 3) == "Dzr") //sprawdza czy beczka jest w dziurze
+        if (manager.GetComponent<GameManager>().iloscRuchow > 0)
         {
-            tilemap.SetTile(GetComponent<Where>().pos, pole); // podmienia pole na którym jest beczka
-            Destroy(gameObject);  //niszczy beczke
+            if (tilemap.GetTile(GetComponent<Where>().pos).name.Substring(0, 3) == "Dzr") //sprawdza czy beczka jest w dziurze
+            {
+                tilemap.SetTile(GetComponent<Where>().pos, pole); // podmienia pole na którym jest beczka
+                Destroy(gameObject);  //niszczy beczke
+            }
         }
+        
     }
 }
